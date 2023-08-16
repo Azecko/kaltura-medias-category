c41307a1b5bdbe36615f3670bee46fdd0aaaf274
Azecko <sami.panseri@epfl.ch>
2023-08-16 17:18:56 +0200

    [feat] getMedias from category
    Log medias id in the console
    Alerting if category id is invalid or empty
    Can't click on submit if input is empty

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ $( document ).ready(function() {
     if(storageObject.adminSecret && storageObject.partnerId) {
         $('.category').removeClass('d-none')
     }
+
+    $('#categoryId-input').on('input',function(e){
+        let inputValue = $('#categoryId-input').val()
+        if(!inputValue) {
+            $('#categoryId-button').prop('disabled', true);
+        } else {
+            $('#categoryId-button').prop('disabled', false);
+        }
+    });
 });
 
 function setLocalStorage(key, value) {
@@ -44,4 +53,24 @@ function setLocalStorage(key, value) {
     } else {
         $('.category').addClass('d-none')
     }
+}
+
+function getMedias(categoryId) {
+    var storageObject = JSON.parse(localStorage.getItem(localStorageName))
+    let adminSecret = storageObject.adminSecret
+    let partnerId = storageObject.partnerId
+
+    createKS(adminSecret, partnerId).then(ks => {
+        getMediasFromCategory(ks, categoryId).then(data => {
+            let media = data.objects
+            if(!media.length) {
+                $('.alert-warning').html("Category doesn't exist or is empty.")
+                $('.alert-warning').removeClass('d-none')
+            }
+
+            data.objects.map(media => {
+                console.log(media.id)
+            })
+        })
+    })
 }
